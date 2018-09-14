9764bf01adca989c5e3d4aaaf0d751d48e898a0d
Paul Fell <swellpf@gmail.com>
2018-09-14 19:07:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
   #   @name = name
   # end  
   
-  def name=(dogname)
-    @dogname=dogname
+  def name=(name)
+    @name=name
   end
   
-  def name(dogname)
-    @dogname
+  def name(name)
+    @name
   end
   
   def bark
