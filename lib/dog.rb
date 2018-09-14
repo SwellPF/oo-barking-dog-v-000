2b830aabfaff61cafa0213f6db29bb1dc16c372e
Paul Fell <swellpf@gmail.com>
2018-09-14 19:06:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  # def initialize(name)
+  #   @name = name
+  # end  
+  
+  def name=(dogname)
+    @dogname=dogname
+  end
+  
+  def name(dogname)
+    @dogname
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
